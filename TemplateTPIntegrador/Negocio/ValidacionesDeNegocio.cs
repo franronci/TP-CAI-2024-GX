0ce48566cc20f901ba62a8c23f6ecadde6d79514
franronci <18RO42689649@campus.economicas.uba.ar>
2024-09-09 19:23:55 -0300

    Metodos de validacion de negocio del nombre de usuario

--- TemplateTPIntegrador/Negocio/ValidacionesDeNegocio.cs
+++ TemplateTPIntegrador/Negocio/ValidacionesDeNegocio.cs
@@ -1,25 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Negocio
 {
     public class ValidacionesDeNegocio
     {
 
-        public Boolean validarLargoUsuario(string nombreUsuario)
+
+        //Metodo que validar que el largo del texto ingresado esta en 8 y 15 caracteres
+
+        public Boolean validarLargoUserPass(string texto_ingresado)
         {
 
-            int largoUsername = nombreUsuario.Length;
-            if (largoUsername < 8)
+            int  = texto_ingresado.Length;
+            if (largo_texto < 8 | largo_texto > 15)
             {
                 return true;
             }
-
             return false;
         }
+
+
+        //Metodo que valida que el usuario no contenga ni el nombre ni el apellido
+        public Boolean validarNombreApellido (string nombre, string apellido, string usuario_ingresado)
+        {
+
+            if (usuario_ingresado.Contains(nombre) || usuario_ingresado.Contains(apellido))
+            {
+                return false; 
+            }
+
+            return true;
+        }
+
+
+    }
+
+
+
 
     }
 }
