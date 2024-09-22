5cf8fe3dcfecf853fd08ebe611ef169d1ace6918
franronci <franciscoronci@gmail.com>
2024-09-22 19:53:20 -0300

    clases

--- TemplateTPIntegrador/Negocio/Administrador.cs
+++ TemplateTPIntegrador/Negocio/Administrador.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Negocio
 {
-    public class Administrador
+    public class Administrador : Usuario
     {
+        // Constructor de la subclase que hereda el constructor de la clase Usuario
+        public Administrador(int host, string nombre, string apellido, int dni, string direccion, string telefono, string email, DateTime fechaNacimiento, string nombreUsuario, string contraseña, string idUsuario) :
+            base(host, nombre, apellido, dni, direccion, telefono, email, fechaNacimiento, nombreUsuario, contraseña, idUsuario)
+        {
+        }
+
+        // Implementación del método abstracto
+        public override void MostrarInformacion()
+        {
+            Console.WriteLine($"Administrador: {Nombre} {Apellido}, DNI: {Dni}, Usuario: {NombreUsuario}");
+        }
     }
+
 }
