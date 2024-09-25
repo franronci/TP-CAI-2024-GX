fa0433ccef31be88ca31c14859dd5c74ac231bdc
franronci <18RO42689649@campus.economicas.uba.ar>
2024-09-24 22:33:46 -0300

    conexion login

---
+++ TemplateTPIntegrador/Negocio/InicioSesion.cs
@@ -0,0 +1,21 @@
+﻿using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class InicioSesion
+    {
+        public string HacerLogin(string username, string password)
+        {
+
+            UsuariosWS usuarios = new UsuariosWS();
+            string id_usuario = usuarios.login(username, password);
+            return id_usuario;
+              
+        }
+    }
+}
---
+++ TemplateTPIntegrador/TemplateTPIntegrador/Login.cs
@@ -0,0 +1,44 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplateTPIntegrador
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            InicioSesion login = new InicioSesion();
+
+            string usuario = txtboxUsernameLogin.Text;
+            string password = txtboxPasswordLogin.Text;
+
+            string id = login.HacerLogin(usuario, password);
+
+            MessageBox.Show(id);
+        }
+    }
+}
