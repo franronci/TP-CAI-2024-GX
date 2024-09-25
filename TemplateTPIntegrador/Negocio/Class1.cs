b2d648d6052bfe85e041db637602cfc2f0593a71
franronci <18RO42689649@campus.economicas.uba.ar>
2024-09-24 21:58:47 -0300

    WS

--- TemplateTPIntegrador/Negocio/Class1.cs
+++ TemplateTPIntegrador/Negocio/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persistencia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,26 @@ namespace Negocio
 {
     public class Class1
     {
+        public void HacerLogin(string username, string password)
+        {
+
+            UsuariosWS usuarios = new UsuariosWS();
+
+            try
+            {
+                usuarios.login(username, password);
+                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error durante el login: " + ex.Message);
+       
+            }
+
+        }
+
+
 
 
     }
