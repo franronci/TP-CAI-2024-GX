d8294a13af34fe518b8371c04f959c9350f385a2
Rodri-333 <rodrigoaragon33@gmail.com>
2024-11-09 18:22:38 -0300

    Se creo logica de negocio para la venta y correspondientes clases en persistencia y datos

---
+++ TemplateTPIntegrador/Datos/DatosVentaWS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DatosVentaWS
+    {
+        string _id;
+
+        public DatosVentaWS()
+        {
+        }
+
+        public DatosVentaWS(string id)
+        {
+            _id = id;
+        }
+        public string Id { get => _id; set => _id = value; }
+    }
+}
---
+++ TemplateTPIntegrador/Datos/VentaPorCliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class VentaPorCliente
+    {
+            string _id;
+            int _cantidad;
+            DateTime _fechaAlta;
+            int _estado;
+
+            public VentaPorCliente()
+            {
+            }
+
+            public VentaPorCliente(string idVenta, int cantidad, DateTime fechaAlta, int estado)
+            {
+                _id = idVenta;
+                _cantidad = cantidad;
+                _fechaAlta = fechaAlta;
+                _estado = estado;
+            }
+            public string Id { get => _id; set => _id = value; }
+            public int Cantidad { get => _cantidad; set => _cantidad = value; }
+            public DateTime FechaAlta { get => _fechaAlta; set => _fechaAlta = value; }
+            public int Estado { get => _estado; set => _estado = value;}
+    }
+}
