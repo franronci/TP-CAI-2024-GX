c114ab9e5326a04fd4ea798a4eb1864e756f5ad4
Rodri-333 <rodrigoaragon33@gmail.com>
2024-11-09 11:55:05 -0300

    Se agrego clase de negocio proveedor y se arreglo proveedorsWS. Ademas se cambio nombre a proveedorws en capa de datos por DatosProveedorWS.

---
+++ TemplateTPIntegrador/Negocio/Proveedor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+using Datos;
+
+namespace Negocio
+{
+    public class Proveedor
+    {
+        private ProveedorWS ProveedorServicio = new ProveedorWS();
+        public String idUsuario = "70b37dc1-8fde-4840-be47-9ababd0ee7e5";
+
+        public List<DatosProveedorWS> listarProveedores()
+        {
+            return ProveedorServicio.TraerProveedores();
+        }
+
+        public void AgregarProveedor(string idUsuario, string Nombre, string Apellido, string CUIT, string email)
+        {
+            AltaProveedor altaProveedor = new AltaProveedor(idUsuario, Nombre, Apellido, CUIT, email);
+            ProveedorServicio.AgregarProveedor(altaProveedor);
+        }
+
+        public void ModificarProveedor(string idProveedor, string idUsuario, string Nombre, string Apellido, string email, string CUIT)
+        {
+            ProveedorServicio.ModificarProveedor(idProveedor, idUsuario, Nombre, Apellido, email, CUIT);
+
+        }
+
+        public void BorrarProveedor(string idProveedor)
+        {
+            ProveedorServicio.bajaProveedor(idProveedor);
+        }
+    }
+}
